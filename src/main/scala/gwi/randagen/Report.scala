b6765b41ec1ef2a7181ed2715473df029a220f81
Jakub Liska <liska.jakub@gmail.com>
2016-02-03 23:50:24 +0100

    Improving report + version bump to 0.7

--- src/main/scala/gwi/randagen/Report.scala
+++ src/main/scala/gwi/randagen/Report.scala
@@ -10,21 +10,26 @@ case class Report(eventCount: Int, producerResponses: List[ProducerResponse], co
     symbols.setGroupingSeparator(' ')
     new DecimalFormat("#,###", symbols)
   }
-  def scale(value: Double) = BigDecimal(value).setScale(2, HALF_UP)
-  def zipByThread(xs: Iterable[Long]) = xs.zipWithIndex.map(t => s"${t._2} thread : ${formatter.format(t._1)} ms").mkString("\n", "\n", "")
   override def toString = {
+    def scale(value: Double) = BigDecimal(value).setScale(2, HALF_UP)
+    def zipByThread(xs: Iterable[Long]) = xs.zipWithIndex.map(t => s"${t._2} thread : ${formatter.format(t._1)} ms").mkString("\n", "\n", "")
+    val slowestProducerTook = producerResponses.map(_.producersTook.toMillis).sorted.last
+
     lazy val generators = zipByThread(producerResponses.map(_.generatorsTook.toMillis))
     lazy val production = zipByThread(producerResponses.map(_.producersTook.toMillis))
     lazy val persistence = formatter.format(consumerResponses.map(_.took).sum)
     lazy val dataSize = scale(consumerResponses.map(_.byteSize.toLong).sum / (1000 * 1000D)).toDouble
-    lazy val throughPut = scale(dataSize / producerResponses.map(_.producersTook.toMillis).sorted.last * 1000D)
+    lazy val dataThroughPut = scale(dataSize / slowestProducerTook * 1000D)
+    lazy val eventThroughPut = scale(eventCount / (slowestProducerTook/1000D))
     s"""
+       |event count : $eventCount
        |event generator creation took : $generators
        |data production took : $production
        |persistence by consumer took : $persistence ms
        |number of batches/files : ${consumerResponses.size}
        |data size : $dataSize MB
-       |throughput : $throughPut MB/s
+       |data throughput : $dataThroughPut MB/s
+       |event throughput : $eventThroughPut events/s
     """.stripMargin
   }
 }
