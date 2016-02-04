992bc69ac882b78f408b64a15c9853476d8ac6cf
Jakub Liska <liska.jakub@gmail.com>
2016-02-04 19:11:09 +0100

    Field Quantity implementation

--- src/main/scala/gwi/randagen/DataSetDefinition.scala
+++ src/main/scala/gwi/randagen/DataSetDefinition.scala
@@ -1,5 +1,7 @@
 package gwi.randagen
 
+import org.apache.commons.math3.distribution.UniformIntegerDistribution
+
 /**
   * Distributions are sampled with progress of iteration mostly because of performance reasons, it allows for data distribution
   * without any CPU ticks. Since event can have thousands of fields, it is obvious that each field cannot hold it's own
@@ -18,6 +20,58 @@ trait EventDef {
 }
 
 /**
+  * Data Set needs to simulate real world cases like :
+  * Impression log with KeyValue query parameters, each of 100 clients having different set of keys
+  * For that you need to virtually split events into 100 segments, each with different # of keys and key names
+  */
+trait FieldQuantity {
+  def names(name: String, p: Progress): Iterator[String] = nameFn(dist.sample(p), name)
+  protected[this] def dist: Distribution[Int]
+  protected[this] def nameFn(n: Int, name: String): Iterator[String]
+}
+
+/**
+  * You need this one in most cases, just a single field, it is the default field quantity
+  */
+case class ConstantQuantity(count: Int) extends FieldQuantity {
+  val dist = Constant(count)
+  def nameFn(n: Int, name: String) = if (count == 1) Iterator(name) else Iterator.range(1, n).map(idx => s"${name}_$idx")
+}
+
+/**
+  * Real world example :
+  * 100 clients with varying number of Keys (up to 1000), will have `maxSegmentCount=100` and `maxFieldCount=1000`
+  */
+trait DynamicQuantity extends FieldQuantity {
+  val dist = DistributedInteger(maxSegmentCount, new UniformIntegerDistribution(1, maxFieldCount))(Parallelism(1))
+  def maxSegmentCount: Int // maximum different field sizes being used for event generation
+  def maxFieldCount: Int // maximum field count possible
+}
+
+/**
+  * Key names are randomly shared among segments
+  */
+case class SharedNamesQuantity(maxSegmentCount: Int, maxFieldCount: Int) extends DynamicQuantity {
+  def nameFn(n: Int, name: String) = Iterator.range(1, n).map(idx => s"${name}_$idx")
+}
+
+/**
+  * Key names are not shared among segments (they are distinct)
+  */
+case class DistinctNamesQuantity(maxSegmentCount: Int, maxFieldCount: Int) extends DynamicQuantity {
+  def nameFn(n: Int, name: String) = {
+    val uniqueSegmentStart = n * n
+    val uniqueSegmentEnd = uniqueSegmentStart + n
+    Iterator.range(uniqueSegmentStart, uniqueSegmentEnd).map(idx => s"${name}_$idx")
+  }
+}
+
+/** If existing Quantities don't cover your edge cases, use CustomQuantity */
+case class CustomQuantity(dist: Distribution[Int])(fn: (Int, String) => Iterator[String]) extends FieldQuantity {
+  def nameFn(n: Int, name: String) = fn(n, name)
+}
+
+/**
   * FieldDef is a definition of a single field/column
   *
   * @note that it has `count` distribution which specifies into how many fields should this definition be expanded to
@@ -25,24 +79,23 @@ trait EventDef {
   * Each field will have it's own distribution - that's why Distribution and Mapper are passed as functions
   */
 object FieldDef {
-  def apply[I,O](name: String, countDist: Distribution[Int], dist: Distribution[I], mapper: Mapper[I,O]): FieldDef = { (progress, format) =>
-    val count = countDist.sample(progress)
-    val names = if (count == 1) Iterator(name) else Iterator.range(1, count).map(idx => s"${name}_$idx")
-    names.map { n =>
-      format match {
-        case _: JsonFormat =>
-          val value = mapper.apply(dist.sample(progress))
-          def formatValue =
-            if (value.isInstanceOf[String])
-              s"""\"$value\""""
-            else
-              value
-          s"""\"$n\": $formatValue"""
-        case _: DsvFormat =>
-          mapper.apply(dist.sample(progress)).toString
-        case x =>
-          throw new IllegalArgumentException(s"Format $x not supported, please use 'json' or 'dsv' !!!")
+  def apply[I,O](name: String, dist: Distribution[I], mapper: Mapper[I,O], quantity: FieldQuantity = ConstantQuantity(1)): FieldDef = { (progress, format) =>
+    quantity.names(name, progress)
+      .map { n =>
+        format match {
+          case _: JsonFormat =>
+            val value = mapper.apply(dist.sample(progress))
+            def formatValue =
+              if (value.isInstanceOf[String])
+                s"""\"$value\""""
+              else
+                value
+            s"""\"$n\": $formatValue"""
+          case _: DsvFormat =>
+            mapper.apply(dist.sample(progress)).toString
+          case x =>
+            throw new IllegalArgumentException(s"Format $x not supported, please use 'json' or 'dsv' !!!")
+        }
       }
-    }
   }
 }
--- src/main/scala/gwi/randagen/SampleEventDef.scala
+++ src/main/scala/gwi/randagen/SampleEventDef.scala
@@ -3,7 +3,7 @@ package gwi.randagen
 import java.time.temporal.ChronoUnit
 import java.time.{LocalDateTime, Month}
 
-import org.apache.commons.math3.distribution.{NormalDistribution, UniformIntegerDistribution, UniformRealDistribution}
+import org.apache.commons.math3.distribution.{NormalDistribution, UniformRealDistribution}
 
 object SampleEventDef extends EventDef {
 
@@ -20,43 +20,43 @@ object SampleEventDef extends EventDef {
     List(
       FieldDef(
         "time",
-        Constant(1),
         Linear,
         TimeMapper("yyyy-MM-dd'T'HH:mm:ss.SSS", ChronoUnit.MILLIS, LocalDateTime.of(2015,Month.JANUARY, 1, 0, 0, 0))
       ),
       FieldDef(
         "gwid",
-        Constant(1),
         Random(50),
         new UuidMapper[Int]
       ),
       FieldDef(
         "country",
-        Constant(1),
         WeightedEnumeration[String](countries),
         new IdentityMapper[String]
       ),
       FieldDef(
         "section",
-        Constant(1),
         DistributedDouble(10000, new NormalDistribution(0D, 0.2)),
         new IdentityMapper[Double]
       ),
       FieldDef(
         "purchase",
-        Constant(1),
         WeightedEnumeration[String](purchase),
         new IdentityMapper[String]
       ),
       FieldDef(
-        "kv",
-        DistributedInteger(100, new UniformIntegerDistribution(1, 1000)),
-        DistributedDouble(12, new NormalDistribution(0D, 0.2)),
-        new IdentityMapper[Double]
+        "kv_shared",
+        DistributedDouble(100, new NormalDistribution(0D, 0.2)),
+        new IdentityMapper[Double],
+        SharedNamesQuantity(12, 20)
+      ),
+      FieldDef(
+        "kv_unique",
+        DistributedDouble(100, new NormalDistribution(0D, 0.2)),
+        new IdentityMapper[Double],
+        DistinctNamesQuantity(12, 20)
       ),
       FieldDef(
         "price",
-        Constant(1),
         DistributedDouble(100, new UniformRealDistribution(1, 1000)),
         new RoundingMapper(2)
       )
