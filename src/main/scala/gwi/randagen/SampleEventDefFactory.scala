047c0343dab11829b2b23a2960d7a6f824db70c0
l15k4 <liska.jakub@gmail.com>
2016-02-29 16:56:32 +0100

    Defined constants for sample data set

--- src/main/scala/gwi/randagen/SampleEventDefFactory.scala
+++ src/main/scala/gwi/randagen/SampleEventDefFactory.scala
@@ -16,53 +16,44 @@ case class SampleEventDefFactory(
       ) extends EventDefFactory {
 
   def apply(implicit p: Parallelism): EventDef = {
-    def purchase = Array("micro" -> 0.1,"small" -> 0.2,"medium" -> 0.4,"large" -> 0.3)
-    def countries = {
-      val list = List(
-        "bra","nzl","bel","bgr","idn","egy","tur","nor","pol","jpn","esp","irl","cze","dnk","che","nld",
-        "ita","rus","pri","deu","eur","pry","usa","dom","gtm","ury","col","fra","isr","arg","mex","gbr"
-      )
-      list.zip(list.foldLeft(List(0.2)) { case (acc, _) => (acc.head * 1.1) :: acc }).toArray
-    }
-
-    val start = LocalDateTime.of(2015,Month.JANUARY, 1, 0, 0, 0)
+    import SampleEventDefFactory._
 
     val pathDef = TimePathDef(Clock("yyyy'/'MM'/'dd'/'HH", tickUnit, start))
 
     val fieldDefs =
       List(
         FieldDef(
-          "time", s"timestamp incremented by one $tickUnit in each event",
+          timeFieldName, s"timestamp incremented by one $tickUnit in each event",
           Linear,
           TimeValueDef(Clock("yyyy-MM-dd'T'HH:mm:ss.SSS", tickUnit, start))
         ),
         FieldDef(
-          "idx", "growing number from 0 until total-number-of-events",
+          idxFieldName, "growing number from 0 until total-number-of-events",
           Linear,
           new IdentityValueDef[Int]
         ),
         FieldDef(
-          "uuid", "randomly generated UUID strings with cardinality 50%",
+          uuidFieldName, "randomly generated UUID strings with cardinality 50%",
           Random(uuidCardinalityRatio),
           new UuidValueDef[Int]
         ),
         FieldDef(
-          "country", "sample of countries with weighted probability distribution",
+          countryFieldName, "sample of countries with weighted probability distribution",
           WeightedEnumeration[String](countries),
           new IdentityValueDef[String]
         ),
         FieldDef(
-          "section", s"normally distributed Doubles sampled from $sectionDataPointsCount distinct values",
+          sectionFieldName, s"normally distributed Doubles sampled from $sectionDataPointsCount distinct values",
           DistributedDouble(sectionDataPointsCount, new NormalDistribution(0D, 0.2)),
           new IdentityValueDef[Double]
         ),
         FieldDef(
-          "purchase", "4 purchase types with weighted probability distribution",
+          purchaseFieldName, "4 purchase types with weighted probability distribution",
           WeightedEnumeration[String](purchase),
           new IdentityValueDef[String]
         ),
         FieldDef(
-          "price", s"randomly distributed Doubles sampled from $priceDataPointsCount distinct values each between 1 and 1000",
+          priceFieldName, s"randomly distributed Doubles sampled from $priceDataPointsCount distinct values each between 1 and 1000",
           DistributedDouble(priceDataPointsCount, new UniformRealDistribution(1, 1000)),
           new RoundingValueDef(2)
         ),
@@ -90,4 +81,25 @@ case class SampleEventDefFactory(
 
     EventDef(fieldDefs, Some(pathDef))
   }
+}
+
+object SampleEventDefFactory {
+  val timeFieldName = "time"
+  val idxFieldName = "idx"
+  val uuidFieldName = "uuid"
+  val countryFieldName = "country"
+  val sectionFieldName = "section"
+  val purchaseFieldName = "purchase"
+  val priceFieldName = "price"
+
+  def purchase = Array("micro" -> 0.1,"small" -> 0.2,"medium" -> 0.4,"large" -> 0.3)
+  def countries = {
+    val list = List(
+      "bra","nzl","bel","bgr","idn","egy","tur","nor","pol","jpn","esp","irl","cze","dnk","che","nld",
+      "ita","rus","pri","deu","eur","pry","usa","dom","gtm","ury","col","fra","isr","arg","mex","gbr"
+    )
+    list.zip(list.foldLeft(List(0.2)) { case (acc, _) => (acc.head * 1.1) :: acc }).toArray
+  }
+
+  val start = LocalDateTime.of(2015,Month.JANUARY, 1, 0, 0, 0)
 }
